aaf86ac0d778a8015fcd05c14b666050f58ee49c
GeekyDeaks <sdeaks@gmail.com>
2023-03-22 14:56:52 +0000

    correct ams2 sample jitter

--- util/ams2dumper.py
+++ util/ams2dumper.py
@@ -26,6 +26,7 @@ freq = 20
 wait = 0
 sample_count = 0
 saved_count = 0
+next_sample = 0
 
 isodate = datetime.datetime.now().replace(microsecond=0).isoformat().replace('-', '').replace(':', '')
 con = sqlite3.connect(os.path.join(PATH, f"ams2-samples-{isodate}.db") )
@@ -39,23 +40,29 @@ while True:
         if wait:
             time.sleep(wait)
 
+        now = time.time() # when
+
         if not shm_b:
             shm_b = shared_memory.SharedMemory("$pcars2$")
+            print("connected to AMS2 shared memory")
+            next_sample = now
 
         v = fmt.unpack(shm_b.buf[:fmt.size])
-        
         sample_count += 1
 
         if v[2] == 2: # mGameState = PLAYING
             saved_count += 1
-            timestamp = int(time.time() * 1000)
-            cur.execute("INSERT INTO samples(timestamp, data) VALUES (?, ?)", (timestamp, shm_b.buf))
+            
+            cur.execute("INSERT INTO samples(timestamp, data) VALUES (?, ?)", (int(now * 1000), shm_b.buf))
 
         if (sample_count % (freq * 10)) == 0:
             print(f"saved {saved_count} samples out of {sample_count}, last: {v}")
             con.commit()
 
-        wait = 1 / freq
+        # work out when the next sample will be
+        now = time.time()
+        next_sample = max(next_sample + (1 / freq), now)
+        wait = next_sample - now
 
     except FileNotFoundError:
         print("waiting for AMS2 to start")
