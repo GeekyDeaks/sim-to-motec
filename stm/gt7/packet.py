1fe735fa4acb336f67153b434af9097d48012c92
GeekyDeaks <sdeaks@gmail.com>
2023-03-22 13:09:24 +0000

    only decode required from gt7

--- stm/gt7/packet.py
+++ stm/gt7/packet.py
@@ -8,42 +8,42 @@ class GT7DataPacket:
         "<"
         "4x" # MAGIC / i / 4x
         "3f" # POSITION / 3f / 12x
-        "3f" # VELOCITY / 3f / 12x
-        "3f" # ROTATION / 3f / 12x
-        "f" # ROTATION_NORTH / f / 4x
-        "3f" # VELOCITY_ANGULAR / 3f / 12x
-        "f" # RIDE_HEIGHT / f / 4x
+        "12x" # VELOCITY / 3f / 12x
+        "12x" # ROTATION / 3f / 12x
+        "4x" # ROTATION_NORTH / f / 4x
+        "12x" # VELOCITY_ANGULAR / 3f / 12x
+        "4x" # RIDE_HEIGHT / f / 4x
         "f" # RPM / f / 4x
         "8x" # IV / 8B / 8x
         "4x" # UNKNOWN_0x48 / f / 4x
         "f" # SPEED / f / 4x
-        "f" # TURBO_BOOST / f / 4x
-        "f" # OIL_PRESSURE / f / 4x
+        "4x" # TURBO_BOOST / f / 4x
+        "4x" # OIL_PRESSURE / f / 4x
         "4x" # UNKNOWN_0x58 / f / 4x
         "4x" # UNKNOWN_0x5C / f / 4x
-        "4f" # TYRES_TEMP / 4f / 16x
+        "16x" # TYRES_TEMP / 4f / 16x
         "i" # TICK / i / 4x
-        "2H" # LAPS / 2H / 4x
+        "2h" # LAPS / 2H / 4x
         "i" # BEST_LAPTIME / i / 4x
         "i" # LAST_LAPTIME / i / 4x
-        "i" # DAYTIME_PROGRESSION / i / 4x
-        "2H" # RACE_POSITION / 2H / 4x
-        "4H" # ALERTS / 4H / 8x
+        "4x" # DAYTIME_PROGRESSION / i / 4x
+        "2h" # RACE_POSITION / 2H / 4x
+        "8x" # ALERTS / 4H / 8x
         "B" # GEAR / B / x
         "B" # THROTTLE / B / x
         "B" # BRAKE / B / x
         "x" # UNKNOWN_0x92 / B / x
-        "4f" # WHEELS_SPEED / 4f / 16x
-        "4f" # TYRES_RADIUS / 4f / 16x
-        "4f" # TYRE_SUSPENSION_TRAVEL / 4f / 16x
+        "16x" # WHEELS_SPEED / 4f / 16x
+        "16x" # TYRES_RADIUS / 4f / 16x
+        "16x" # TYRE_SUSPENSION_TRAVEL / 4f / 16x
         "16x" # UNKNOWN / 4f / 16x
         "32x" # UNKNOWN_RESRVED / 32B / 32x
-        "f" # CLUCH / f / 4x
-        "f" # CLUCH_ENGAGEMENT / f / 4x
-        "f" # CLUCH_RPM / f / 4x
+        "4x" # CLUCH / f / 4x
+        "4x" # CLUCH_ENGAGEMENT / f / 4x
+        "4x" # CLUCH_RPM / f / 4x
         "4x" # UNKNOWN_GEAR / f / 4x
         "32x" # UNKNOWN_GEAR_RATIO / 8f / 32x
-        "i" # CAR_CODE / i / 4x
+        "I" # CAR_CODE / i / 4x
     )
 
     def __init__(self, buf, encrypted=True):
@@ -55,17 +55,19 @@ class GT7DataPacket:
             self.positionX,
             self.positionY,
             self.positionZ,
-            self.velocityX,
-            self.velocityY,
-            self.velocityZ,
-            self.rotationX,
-            self.rotationY,
-            self.rotationZ,
-            self.rotation_north,
-            self.angular_velocityX,
-            self.angular_velocityY,
-            self.angular_velocityZ,
-            *self.t  # remaining stuff
+            self.rpm,
+            self.speed,
+            self.tick,
+            self.current_lap,
+            self.laps,
+            self.best_laptime,
+            self.last_laptime,
+            self.race_position,
+            self.opponents,
+            self.gear,
+            self.throttle,
+            self.brake,
+            self.car_code
         )  = self.fmt.unpack(buf)
 
 
