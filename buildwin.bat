cc4c2b60d14902304c435a895b0a174ebb64bace
GeekyDeaks <sdeaks@gmail.com>
2023-07-29 19:44:27 +0100

    zip MotecWorkspace during build

--- buildwin.bat
+++ buildwin.bat
@@ -1,2 +1,3 @@
 pyinstaller --noconsole --onefile ams2.py
-pyinstaller --noconsole --add-data stm\gt7\db\*.csv;stm\gt7\db --onefile gt7.py 
+pyinstaller --noconsole --add-data stm\gt7\db\*.csv;stm\gt7\db --onefile gt7.py
+tar acvf dist\MotecWorkspace.zip MotecWorkspace
