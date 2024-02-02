c7cedc674e1e2cd26b5729785dad93bda870cec9
GeekyDeaks <sdeaks@gmail.com>
2024-02-02 16:47:17 +0000

    added LeMans track GPS for AMS2

--- buildwin.bat
+++ buildwin.bat
@@ -1,2 +1,2 @@
-pyinstaller --noconsole --onefile ams2.py
+pyinstaller --noconsole --add-data stm\ams2\tracks\*.json;stm\ams2\tracks --onefile ams2.py
 pyinstaller --noconsole --add-data stm\gt7\db\*.csv;stm\gt7\db --onefile gt7.py 
